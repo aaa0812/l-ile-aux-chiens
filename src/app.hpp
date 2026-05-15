e6f2356a7590df72ed12185cd199d3e302d6d235
Elea Poirot <contact.elea.poirot@gmail.com>
2026-05-15 17:28:13 +0200

    feat : ajout paramètres pour la génération de points

--- src/app.hpp
+++ src/app.hpp
@@ -4,40 +4,47 @@
 #include "glm/glm.hpp"
 #include <vector>
 
-struct ImageGenerationParameters {
-    int noiseSeed { 0 };
-    float noiseScale { 4.0f };
-    int resolution { 256 };
+struct ImageGenerationParameters
+{
+    int noiseSeed{0};
+    float noiseScale{4.0f};
+    int resolution{256};
 };
 
-struct PointsGenerationParameters {
+struct PointsGenerationParameters
+{
     // TODO(student): add parameters for points generation (ex: poisson disk radius, etc).
+
+    float r{0.01}; // minimum disctance between samples
+    int limit{30};       // limit of samples to choose before rejection (constant k)
+    float cellSize = r / sqrt(2); // We pick the cell size to be bounded by r/√n, so that each grid cell will contain at most one sample
 };
 
-struct AppContext {
-    Camera camera {};
+struct AppContext
+{
+    Camera camera{};
 
     // Store the heightmap as a raylib Image, which is easy to sample from CPU side when generating object positions.
-    Image heightmapImage {};
+    Image heightmapImage{};
 
     // This is the image we use for texturing the terrain. It can be the same as heightmapImage, but it doesn't have to be (for example, you could use a color image with RGB channels representing different material types instead of height).
-    Image image {};
+    Image image{};
 
     // The generated texture from the image, stored here so we can easily bind it when generating the model.
-    Texture2D texture {};
+    Texture2D texture{};
 
-    glm::vec3 terrainSize { 16.0f, 5.0f, 16.0f };
+    glm::vec3 terrainSize{16.0f, 5.0f, 16.0f};
 
     // The generated terrain mesh and model.
-    Mesh mesh {};
-    Model model {};
+    Mesh mesh{};
+    Model model{};
 
-    std::vector<glm::vec3> objectPositions {};
+    std::vector<glm::vec3> objectPositions{};
 
     // A simple cube mesh and material we use to draw objects on the terrain.
-    Mesh cube {};
-    Material cubeMaterial {};
-    float cubeScale { 0.1f };
+    Mesh cube{};
+    Material cubeMaterial{};
+    float cubeScale{0.1f};
 
     // Parameters for object positions generation
     PointsGenerationParameters pointsGenerationParameters;
@@ -46,7 +53,7 @@ struct AppContext {
     ImageGenerationParameters imageGenerationParameters;
 };
 
-Matrix getTerrainCenteringMatrix(AppContext const& context);
-float sampleHeightmap(AppContext const& context, float u, float v);
-void unload(AppContext& context);
-void regenerateMeshFromImage(AppContext& context);
+Matrix getTerrainCenteringMatrix(AppContext const &context);
+float sampleHeightmap(AppContext const &context, float u, float v);
+void unload(AppContext &context);
+void regenerateMeshFromImage(AppContext &context);
