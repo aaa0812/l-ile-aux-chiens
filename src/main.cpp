718ec364ea8a20ed4a6b16527f994c31fb51c11e
Elea Poirot <contact.elea.poirot@gmail.com>
2026-06-01 10:39:14 +0200

    feat : texture sur le sucre d'orge

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@
 #include <filesystem>
 #include "utils/pathUtils.hpp"
 
-int main() {
+int main()
+{
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(1280, 720, "IMAC island viewer");
 
@@ -16,31 +17,31 @@ int main() {
     ClearWindowState(FLAG_FULLSCREEN_MODE);
     SetWindowMinSize(800, 450);
 
-    int const monitor { GetCurrentMonitor() };
-    int const monitor_width { GetMonitorWidth(monitor) };
-    int const monitor_height { GetMonitorHeight(monitor) };
-    if (monitor_width > 0 && monitor_height > 0) {
-        SetWindowSize(monitor_width/2, monitor_height/2);
-        SetWindowPosition(monitor_width/4, monitor_height/4);
+    int const monitor{GetCurrentMonitor()};
+    int const monitor_width{GetMonitorWidth(monitor)};
+    int const monitor_height{GetMonitorHeight(monitor)};
+    if (monitor_width > 0 && monitor_height > 0)
+    {
+        SetWindowSize(monitor_width / 2, monitor_height / 2);
+        SetWindowPosition(monitor_width / 4, monitor_height / 4);
     }
     SetTargetFPS(30);
 
     rlImGuiSetup(true);
 
-    AppContext context {};
+    AppContext context{};
 
     context.cube = GenMeshCube(1.0f, 1.0f, 1.0f);
     context.cubeMaterial = LoadMaterialDefault();
     context.cubeMaterial.maps[MATERIAL_MAP_DIFFUSE].color = RED;
-    
+
     // Define our custom camera to look into our 3d world
     context.camera = {
-        .position={ 18.0f, 21.0f, 18.0f },
-        .target={ 0.0f, 0.0f, 0.0f },
-        .up={ 0.0f, 1.0f, 0.0f },
-        .fovy=45.0f,
-        .projection=CAMERA_PERSPECTIVE
-    };
+        .position = {18.0f, 21.0f, 18.0f},
+        .target = {0.0f, 0.0f, 0.0f},
+        .up = {0.0f, 1.0f, 0.0f},
+        .fovy = 45.0f,
+        .projection = CAMERA_PERSPECTIVE};
 
     // loading texture from file exemple, not used for now since we generate the heightmap texture from code, but it can be useful for testing or as a fallback
     // std::filesystem::path path { pathUtils::make_absolute_path("resources/heightmap.png") };
@@ -50,31 +51,35 @@ int main() {
     // context.model = LoadModelFromMesh(context.mesh);                  // Load model from generated mesh
     // context.model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = context.texture; // Set map diffuse texture
     // std::filesystem::path path { pathUtils::make_absolute_path("resources/chien.glb") };
-    /* std::filesystem::path path { pathUtils::make_absolute_path("resources/chien.glb") };
+    /*
     std::filesystem::path textPath { pathUtils::make_absolute_path("resources/heightmap.png") };
     context.dog = LoadModel(path.string().c_str());
-    Texture2D texture = LoadTexture(textPath.string().c_str());         // Load model texture and set material
+    Texture2D texture = LoadTexture(textPath.string().c_str());
     context.dog.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture; */
-    
-    std::filesystem::path treePath { pathUtils::make_absolute_path("resources/models/dead-tree.glb") };
+
+    std::filesystem::path treePath{pathUtils::make_absolute_path("resources/models/dead-tree.glb")};
     context.winterTree1 = LoadModel(treePath.string().c_str());
 
-    std::filesystem::path tree2Path { pathUtils::make_absolute_path("resources/models/dead-tree2.glb") };
+    std::filesystem::path tree2Path{pathUtils::make_absolute_path("resources/models/dead-tree2.glb")};
     context.winterTree2 = LoadModel(tree2Path.string().c_str());
 
-    std::filesystem::path boatPath { pathUtils::make_absolute_path("resources/boat.glb") };
+    std::filesystem::path boatPath{pathUtils::make_absolute_path("resources/boat.glb")};
     context.boat = LoadModel(boatPath.string().c_str());
-    
-    std::filesystem::path candycanePath { pathUtils::make_absolute_path("resources/models/candy.glb") };
-    std::filesystem::path lolipopPath { pathUtils::make_absolute_path("resources/models/lolipop.glb") };
+
+    std::filesystem::path candycanePath{pathUtils::make_absolute_path("resources/models/candy-cane.obj")};
+    std::filesystem::path lolipopPath{pathUtils::make_absolute_path("resources/models/lolipop.glb")};
     context.candyCane = LoadModel(candycanePath.string().c_str());
     context.lolipop = LoadModel(lolipopPath.string().c_str());
+
+    std::filesystem::path textPath{pathUtils::make_absolute_path("resources/models/candy-text.png")};
+    Texture2D candyCaneText = LoadTexture(textPath.string().c_str()); // Load model texture and set material
+    context.candyCane.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = candyCaneText;
 
     generateHeightmap(context);
     regenerateMeshFromImage(context);
     generateObjectsPositions(context);
 
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         UpdateCamera(&context.camera, CAMERA_ORBITAL);
         BeginDrawing();
